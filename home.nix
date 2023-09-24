a950f21a1f2e607656b3a96027dc83aec1b12100
Robert Schambach <scha@in.tum.de>
2023-09-24 13:11:48 +0200

    Add vimconf

--- home.nix
+++ home.nix
@@ -57,6 +57,92 @@
 
 	" autocmd FileType yaml ts=2 sts=2 sw=2 expandtab
 	" autocmd FileType nix ts=2 sts=2 sw=2 expandtab
+
+        set directory^=$HOME/.config/nvim/vimswap//
+        set fsync
+
+        set ruler	
+
+        set undolevels=1000	
+        set backspace=indent,eol,start	
+
+        set undofile
+        set undodir=/home/robert/.config/nvim/.vimundo/
+
+        call plug#begin('~/.local/share/nvim/plugged')
+
+        Plug 'cespare/vim-toml'
+        Plug 'https://tpope.io/vim/surround.git'
+        Plug 'svermeulen/vim-yoink'
+        Plug 'tpope/vim-repeat'
+        Plug 'svermeulen/vim-cutlass'
+        Plug 'aliou/bats.vim'
+        Plug 'vim-scripts/LargeFile'
+        "Plug 'vim-syntastic/syntastic'
+        Plug 'rust-lang/rust.vim'
+        Plug 'junegunn/vim-easy-align'
+        Plug 'airblade/vim-gitgutter'
+        Plug 'NoahTheDuke/vim-just'
+        "Plug 'neovim/nvim-lspconfig'
+        " :GrammarousCheck --help
+        " :[range]GrammarousCheck [--lang={lang}] [--(no-)preview] [--(no-)comments-only]
+
+
+        call plug#end()
+
+
+        "vim-easy-align config
+        " Start interactive EasyAlign in visual mode (e.g. vipga)
+        xmap ga <Plug>(EasyAlign)
+
+        " Start interactive EasyAlign for a motion/text object (e.g. gaip)
+        nmap ga <Plug>(EasyAlign)
+
+
+        "bats.vim config
+        let g:bats_vim_consider_dollar_as_part_of_word = 0
+
+
+        "vim-yoink config
+        "swap most recent paste around in yank history
+        nmap <c-n> <plug>(YoinkPostPasteSwapBack)
+        nmap <c-p> <plug>(YoinkPostPasteSwapForward)
+
+        "use vimyank for pasting
+        nmap p <plug>(YoinkPaste_p)
+        nmap P <plug>(YoinkPaste_P)
+
+        "permanently cycle through history
+        nmap [y <plug>(YoinkRotateBack)
+        nmap ]y <plug>(YoinkRotateForward)
+
+        "toggle if current paste is formatted
+        nmap <c-=> <plug>(YoinkPostPasteToggleFormat)
+
+        "vimcutlass: add cut operations to Yank history
+        let g:yoinkIncludeDeleteOperations = 1
+
+        "remove highlight
+        nnoremap <silent> <esc> :noh<cr><esc>
+
+        "syntastic: recommended defaults
+        "set statusline+=%#warningmsg#
+        "set statusline+=%{SyntasticStatuslineFlag()}
+        "set statusline+=%*
+        "
+        "let g:syntastic_always_populate_loc_list = 1
+        "let g:syntastic_auto_loc_list = 1
+        "let g:syntastic_check_on_open = 1
+        "let g:syntastic_check_on_wq = 0
+
+        "vim-gitgutter config
+        set updatetime=100
+        "GitGutterLineNrHighlightsEnable
+
+        "lua-specific config
+        " - lsp
+        "NOTE: requires neovim v0.5
+        "lua require('init')
       '';
     };
     direnv =
